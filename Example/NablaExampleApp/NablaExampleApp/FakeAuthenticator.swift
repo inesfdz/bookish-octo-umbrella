ce2ee70102dd0e79593b71f2c0164cf1b2ed526f
Inès Fernandez <36076515+inesfdz@users.noreply.github.com>
2022-07-05 14:29:28 +0200

    Update FakeAuthenticator.swift

--- Example/NablaExampleApp/NablaExampleApp/FakeAuthenticator.swift
+++ Example/NablaExampleApp/NablaExampleApp/FakeAuthenticator.swift
@@ -8,8 +8,8 @@ class FakeAuthenticator: SessionTokenProvider {
         // Emulate a call to authenticate the user on your server
         // In your app, you need to replace this with an actual call to your backend to get fresh tokens
         completion(.init(
-            accessToken: "<youraccesstoken>",
-            refreshToken: "<yourrefreshtoken>"
+            accessToken: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIwNWUzM2FmMC02M2I1LTRlZDctOWIxMi00ZDcxOGE4YmI3NWMiLCJpc3MiOiJwcm9kLXBhdGllbnQiLCJ0eXAiOiJCZWFyZXIiLCJleHAiOjE2NTcwMjM2MTQsInNlc3Npb25fdXVpZCI6IjIzZjA4OGY0LTQ3MzgtNGQyYi1iMzUzLTdiYzcwYjA0ZmViMyIsIm9yZ2FuaXphdGlvblN0cmluZ0lkIjoiaGlubGFiMyJ9.cemYBBwW3K11PM03LzDyYF3eUkT_g7-9nQPWsh6o3Ec",
+            refreshToken: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIwNWUzM2FmMC02M2I1LTRlZDctOWIxMi00ZDcxOGE4YmI3NWMiLCJpc3MiOiJwcm9kLXBhdGllbnQiLCJ0eXAiOiJSZWZyZXNoIiwiZXhwIjoxNjY0Nzk5MzE0LCJzZXNzaW9uX3V1aWQiOiIyM2YwODhmNC00NzM4LTRkMmItYjM1My03YmM3MGIwNGZlYjMiLCJvcmdhbml6YXRpb25TdHJpbmdJZCI6ImhpbmxhYjMifQ.uU3aHMNFKAWv3fBYS7AsO_KSv_Mdu3cH4dzZvPsOA0E"
         ))
     }
 }
